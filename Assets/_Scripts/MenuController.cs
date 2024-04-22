676a7bf7220958418367167510dfa1df68f8527f
Alexander Evstratov <alexdrugev@me.com>
2024-04-23 01:35:41 +0300

    Add menu panels

--- Assets/_Scripts/MenuController.cs
+++ Assets/_Scripts/MenuController.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject _levelsPanel;
+    [SerializeField] private GameObject _skinsPanel;
+    [SerializeField] private GameObject _settingsPanel;
     private void Start()
     {
         Time.timeScale = 1;
@@ -14,5 +17,40 @@ public class MenuController : MonoBehaviour
     public void PlayBtn()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenLevelsPanel()
+    {
+        _levelsPanel.SetActive(true);
+    }
+
+    public void CloseLevelsPanel()
+    {
+        _levelsPanel.SetActive(false);
+    }
+
+    public void OpenSkinsPanel()
+    {
+        _skinsPanel.SetActive(true);
+    }
+
+    public void CloseSkinsPanel()
+    {
+        _skinsPanel.SetActive(false);
+    }
+
+    public void OpenSettingsPanel()
+    {
+        _settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        _settingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
